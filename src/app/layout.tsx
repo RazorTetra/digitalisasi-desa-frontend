8d95c9cdf80a13824ca1fa1b03e1a1a7c99a9c90
toniliu <toniliu672@gmail.com>
2024-10-25 19:13:28 +0800

    refactor(favicon, layoutMain): remove manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,18 +77,18 @@ export const metadata: Metadata = {
       url: "/apple-touch-icon-precomposed.png",
     },
   },
-  manifest: "/site.webmanifest",
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
+  // manifest: "/site.webmanifest",
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //     "max-video-preview": -1,
+  //     "max-image-preview": "large",
+  //     "max-snippet": -1,
+  //   },
+  // },
 };
 
 export default function RootLayout({
